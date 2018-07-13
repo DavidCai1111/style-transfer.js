2e443b4c128c11482a50f5f664a62be9d7bca86d
David Cai <davidcai1993@yahoo.com>
2018-07-13 11:30:39 +0800

    feat: add computeLayerStyleCost()

--- cost.js
+++ cost.js
@@ -9,10 +9,45 @@ function computeContentCost (rawContentActivation, generatedContentActivation) {
 
   const contentCost = tf.mul(
     (1 / (4 * nH * nW * nC)),
-    tf.pow((generatedContentActivationUnrolled.sub(rawContentActivationUnrolled)), 2).sum()
+    tf.pow(
+      generatedContentActivationUnrolled.sub(rawContentActivationUnrolled), 2
+    ).sum()
   )
 
   return contentCost
 }
 
-module.exports = { computeContentCost }
+function computeGramMatrix (Activation) {
+  const GramMatrix = tf.matMul(Activation, tf.transpose(Activation))
+
+  return GramMatrix
+}
+
+function computeLayerStyleCost (rawContentActivation, generatedContentActivation) {
+  const [, nH, nW, nC] = generatedContentActivation.shape
+
+  rawContentActivation = tf.transpose(
+    tf.reshape(rawContentActivation, [nH * nW, nC])
+  )
+  generatedContentActivation = tf.transpose(
+    tf.reshape(generatedContentActivation, [nH * nW, nC])
+  )
+
+  const rawContentGramMatrix = computeGramMatrix(rawContentActivation)
+  const generatedContentGramMatrix = computeGramMatrix(generatedContentActivation)
+
+  const layerStyleCost = tf.mul(
+    (1 / (4 * (nH ** 2) * ((nH * nW) ** 2))),
+    tf.pow(
+      rawContentGramMatrix.sub(generatedContentGramMatrix), 2
+    ).sum()
+  )
+
+  return layerStyleCost
+}
+
+module.exports = {
+  computeContentCost,
+  computeGramMatrix,
+  computeLayerStyleCost
+}
--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 'use strict'
 require('@tensorflow/tfjs-node')
 const tf = require('@tensorflow/tfjs')
-const { computeContentCost } = require('./cost')
+const { computeContentCost, computeGramMatrix, computeLayerStyleCost } = require('./cost')
 
 ;(async function () {
-  // const vgg19 = await tf.loadModel(`file://${__dirname}/vgg19-tensorflowjs-model/model/model.json`)
-
-  const aC = tf.randomNormal([1, 4, 4, 3], 1, 4)
-  const aG = tf.randomNormal([1, 4, 4, 3], 1, 4)
-
-  aC.print()
-  console.log(computeContentCost(aC, aG).print())
+  const vgg19 = await tf.loadModel(`file://${__dirname}/vgg19-tensorflowjs-model/model/model.json`)
 })(console.error)
