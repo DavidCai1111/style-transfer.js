2c60c2c93d36373e3e305eb1c0da499a80b44e45
David Cai <davidcai1993@yahoo.com>
2018-07-16 22:37:58 +0800

    feat: optimize cost functions

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const cost = require('./lib/cost')
 const util = require('./lib/util')
 const { getLayerResult } = require('./lib/layer')
 
+tf.setBackend('tensorflow')
+
 ;(async function () {
   const vgg19 = await tf.loadModel(`file://${__dirname}/vgg19-tensorflowjs-model/model/model.json`)
 
   const currentImage = await util.loadImage('./images/louvre.jpg')
   const styleImage = await util.loadImage('./images/monet_800600.jpg')
-  const outputImage = util.generateNoiseImage(currentImage)
 
-  function loss () {
-    const rawActivation = getLayerResult(vgg19, currentImage, 'block4_conv2')
-    const generatedActivation = getLayerResult(vgg19, outputImage, 'block4_conv2')
+  const rawActivation = getLayerResult(vgg19, currentImage, 'block4_conv2')
+  let outputImage = util.generateNoiseImage(currentImage)
 
-    const contentCost = cost.computeContentCost(rawActivation, generatedActivation)
+  const loss = () => {
+    const contentCost = cost.computeContentCost(
+      rawActivation,
+      getLayerResult(vgg19, outputImage, 'block4_conv2')
+    )
+
     const styleCost = cost.computeStyleCost(vgg19, styleImage, outputImage)
-
     const totalCost = cost.computeTotalCost(contentCost, styleCost, 10, 40)
 
     console.log({
@@ -32,7 +36,11 @@ const { getLayerResult } = require('./lib/layer')
 
   const optimizer = tf.train.adam(2)
 
-  for (let i = 0; i < 80; i++) {
-    optimizer.minimize(() => loss())
+  for (let i = 0; i < 200; i++) {
+    const start = Date.now()
+    console.log({ outputImage: outputImage.dataSync() })
+    const cost = optimizer.minimize(() => loss(), true, [outputImage])
+    console.log({ outputImage: outputImage.dataSync() })
+    console.log(`cost: ${cost.dataSync()}, use ${(Date.now() - start) / 1000}s`)
   }
 })(console.error)
