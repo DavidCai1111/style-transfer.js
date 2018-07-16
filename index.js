13e31f9c157651ab38770f21151fd098d770da45
David Cai <davidcai1993@yahoo.com>
2018-07-16 11:55:29 +0800

    feat: define train step

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 require('@tensorflow/tfjs-node')
 const tf = require('@tensorflow/tfjs')
 const cost = require('./lib/cost')
-const { loadImage } = require('./lib/util')
+const util = require('./lib/util')
 const { getLayerResult } = require('./lib/layer')
 
 ;(async function () {
   const vgg19 = await tf.loadModel(`file://${__dirname}/vgg19-tensorflowjs-model/model/model.json`)
 
-  const currentImage = await loadImage('./images/louvre.jpg')
-  const styleImage = await loadImage('./images/monet_800600.jpg')
+  const currentImage = await util.loadImage('./images/louvre.jpg')
+  const styleImage = await util.loadImage('./images/monet_800600.jpg')
+  const noiseImage = util.generateNoiseImage(currentImage)
 
   console.log({ currentImage, styleImage })
 
@@ -19,5 +20,10 @@ const { getLayerResult } = require('./lib/layer')
   const contentCost = cost.computeContentCost(rawActivation, generatedActivation)
   const styleCost = cost.computeStyleCost(vgg19, styleImage)
 
-  console.log({ contentCost, styleCost })
+  const totalCost = cost.computeTotalCost(contentCost, styleCost, 10, 40)
+
+  console.log({ contentCost, styleCost, totalCost })
+
+  const optimizer = tf.train.adam(2)
+  const trainStep = optimizer.minimize(totalCost)
 })(console.error)
