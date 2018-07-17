3e112ec743b43dc67e66c0fc1fd2d9b8d3c9b38d
David Cai <davidcai1993@yahoo.com>
2018-07-17 14:35:09 +0800

    feat: use commander

---
+++ transfer.js
@@ -0,0 +1,18 @@
+'use strict'
+require('@tensorflow/tfjs-node')
+const transfer = require('commander')
+const pkg = require('./package')
+const model = require('./lib/model')
+
+transfer.version(pkg.version)
+
+transfer
+  .command('transfer <contentImagePath> <styleImagePath> <outputImagePath>')
+  .description('tranfer the style of the "content image"')
+  .action(function (contentImagePath, styleImagePath, outputImagePath) {
+    ;(async function () {
+      await model.run(contentImagePath, styleImagePath, outputImagePath)
+    })(console.error)
+  })
+
+transfer.parse(process.argv)
